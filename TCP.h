a8477547c28e898cdc33404a4263d4d374ecec12
larusx <larusx@163.com>
2013-05-29 23:39:21 +0800

    init

---
+++ TCP.h
@@ -0,0 +1,17 @@
+#ifndef _TCP_H
+#define _TCP_H
+#include<sys/socket.h>
+namespace X
+{
+class TCP
+{
+public:
+	static getTCP(int port)
+	{
+
+	}
+
+	
+}
+}
+#endif
---
+++ Xproject.h
@@ -0,0 +1,13 @@
+#include<sys/socket.h>
+#include<unistd.h>
+#include<fcntl.h>
+namespace X
+{
+	class parse_http;
+	class proc_GET;
+	class proc_POST;
+	class proc_upload;
+	class proc_websocket;
+	class TCP;
+
+}
